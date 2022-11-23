9e0b33c6183d69df581df37931f946e88fd654b0
Jusi <megamahmutpower@gmail.com>
2022-11-23 23:19:01 +0300

    start

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+function Header() {
+    return (
+        <header className="header">
+            <div className="nav__wrapper">
+                <div className="logo">
+
+                </div>
+                <div className="nav__link">
+                    <a className="" href="#">
+
+                    </a>
+                </div>
+            </div>
+        </header>
+    );
+    }
+    
+export default Header;
