5763d505628354adb4460c459ca5fe350bcca994
Jusi <megamahmutpower@gmail.com>
2022-11-27 23:23:30 +0300

    base card construction

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,26 @@ function Header() {
                 <div className="logo">
 
                 </div>
-                <div className="nav__link">
-                    <a className="" href="#">
-
+                <div className="nav__links">
+                    <a className="link" href="#">
+                        Главная
+                    </a>
+                    <a className="link" href="#">
+                        Маркетплейс
+                    </a>
+                    <a className="link" href="#">
+                        Сообщество
+                    </a>
+                </div>
+                <div className="auth__link">
+                    <a className="link" href="#">
+                        Вход
+                    </a>
+                    <a className="link" href="#">
+                        Регистрация
+                    </a>
+                    <a className="link" href="#">
+                        RU
                     </a>
                 </div>
             </div>
---
+++ src/components/Main.js
@@ -0,0 +1,53 @@
+function Main() {
+    return (
+        <main>
+            <div className="main__wrap">
+                <div className="popular__collection">
+                    <div className="title__wrap">
+                        <h3 className="title">Популярные коллекции</h3>
+                        <a className="title__link" href="#">Больше</a>
+                    </div>
+                    <div className="popular__cards">
+                        <div className="popular__card">
+                            <div className="img__wrap">
+                                <div className="img">
+
+                                </div>
+
+                                <div className="avatar">
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="popular__card">
+                            <div className="img__wrap">
+                                <div className="img">
+
+                                </div>
+
+                                <div className="avatar">
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="popular__card">
+                            <div className="img__wrap">
+                                <div className="img">
+
+                                </div>
+
+                                <div className="avatar">
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
+    }
+    
+export default Main;
